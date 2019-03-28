00b03860de48e6ca0f82f4c4a5acfd88428641a6
JJongSue <jspark940321@naver.com>
2019-03-29 00:37:58 +0900

    1206 추가

--- JJongSue/1206/solution.cpp
+++ JJongSue/1206/solution.cpp
@@ -1,30 +1,35 @@
 #include <iostream>
-#include <deque>
 #include <algorithm>
 using namespace std;
+int four_max(int a, int b, int c ,int d);
 int main(void)
 {
-
     for(int i=0;i<10;i++)
     {
-        int testcase_num=0, left_one=0, left_two=0, right_one, right_two, center=0, sun=0;
+        int testcase_num=0, left_one=0, left_two=0, right_one=0, right_two=0, center=0, sun=0;
         cin >> testcase_num;
+        cin >> left_two;
+        cin >> center;
         cin >> right_one;
         cin >> right_two;
-        for(int j=2;j<testcase_num;j++)
+        int max=four_max(left_one, left_two, right_one, right_two);
+        if( center > max ) sun += center - max;
+        for(int j=5;j<=testcase_num;j++)
         {
             left_one = left_two;
             left_two = center;
             center=right_one;
+            right_one = right_two;
             cin >> right_two;
             int max=four_max(left_one, left_two, right_one, right_two);
             if( center > max ) sun += center - max;
         }
+        cout << "#" << i+1 << " " << sun << endl; 
     }
     return 0;
 }
 int four_max(int a, int b, int c ,int d)
 {
-    if(max(a,b) >= max(c,d) ) return max(a,b);
-    else return max(c,d);
+    if(max(a,b) >= max(c,d) ) return (max(a,b));
+    else return (max(c,d)) ;
 }
