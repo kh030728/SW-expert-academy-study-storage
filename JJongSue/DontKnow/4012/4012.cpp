a07ed3160f8d6e2e00cfa7261b46c4779acdfd4e
박종수 <jspark940321@gmail.com>
2019-08-07 18:07:59 +0900

    f

--- JJongSue/DontKnow/4012/4012.cpp
+++ JJongSue/DontKnow/4012/4012.cpp
@@ -1,6 +1,6 @@
-#include<iostream>
-#include<vector>
-#include<math.h>
+#include <iostream>
+#include <vector>
+#include <math.h>
 using namespace std;
 
 vector<vector<int>> s;
@@ -9,56 +9,70 @@ static int min1;
 void find(int now, int cnt, vector<bool> is_visit);
 int main(void)
 {
-    int test=0;
+    int test = 0;
     scanf("%d", &test);
-    for(int test_i=1;test_i<=test;test_i++)
+    for (int test_i = 1; test_i <= test; test_i++)
     {
-        min1=160000;
+        min1 = 160000;
         scanf("%d", &N);
-        vector<bool> is_visit(N,false);
-        s.assign(N, vector<int>(N,0));
-        for(int i=0;i<N;i++)
+        vector<bool> is_visit(N, false);
+        s.clear();
+        s.assign(N, vector<int>(N, 0));
+        for (int i = 0; i < N; i++)
         {
-            for(int j=0;j<N;j++)
+            for (int j = 0; j < N; j++)
             {
                 scanf("%d", &s[i][j]);
             }
         }
-        find(0,0,is_visit);
-        printf("#%d %d", test_i, min1);
-
+        find(0, 0, is_visit);
+        printf("#%d %d\n", test_i, min1);
     }
     return 0;
 }
 void find(int now, int cnt, vector<bool> is_visit)
 {
-    if(now>=N) return;
-    if( cnt == N/2)
+
+    if (cnt == N / 2)
     {
-        int A=0, B=0;
-        for(int i=0;i<N-1;i++)
+        int A = 0, B = 0;
+        for (int i = 0; i < N; i++)
         {
-            for(int j=i+1;j<N;j++)
+            if (is_visit[i] == true)
             {
-                if( is_visit[i] == true && is_visit[j] == true )
+                for (int j = 0; j < N; j++)
+                {
+                    if ((is_visit[j] == true))
                     {
-                        A=s[i][j]+s[j][i];                        
+                        A += s[i][j];
                     }
-                    else if(is_visit[i] == false && is_visit[j] == false)
+                }
+            }
+            else
+            {
+                for (int j = 0; j < N; j++)
+                {
+                    if ((is_visit[j] == false))
                     {
-                        B=s[i][j]+s[j][i];  
+                        B += s[i][j];
                     }
+                }
             }
         }
-        if(abs(A-B) < min1) min1=abs(A-B);
+        if (abs(A - B) < min1)
+            min1 = abs(A - B);
         return;
     }
-    else{
-        find(now+1, cnt, is_visit);
-        vector<bool> is_visit2=is_visit;
-        is_visit2[now]=true;
-        find(now+1, cnt+1, is_visit2);
+    else
+    {
+        if (now >= N)
+            return;
+        if (cnt > N / 2)
+            return;
+        vector<bool> is_visit2 = is_visit;
+        find(now + 1, cnt, is_visit2);
 
+        is_visit2[now] = true;
+        find(now + 1, cnt + 1, is_visit2);
     }
-
 }
