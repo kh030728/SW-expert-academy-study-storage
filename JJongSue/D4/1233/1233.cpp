8deb8ed6f396aa42c31ffcb421271b9fc8898463
박종수 <jspark940321@gmail.com>
2019-08-12 19:54:16 +0900

    190812

---
+++ JJongSue/D4/1233/1233.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+
+using namespace std;
+int main(void)
+{
+    int test=0;
+    scanf("%d", &test);
+    for(int test_i=1;test_i<=test;test_i++)
+    {
+        int N =0;
+        scanf("%d", &N);
+        
+    }
+    return 0;
+}
